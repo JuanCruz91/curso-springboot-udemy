85551a36e3159d92660a13c96daa1eb52941159f
juanchitovilla2 <104386449+juanchitovilla2@users.noreply.github.com>
2025-10-16 20:09:36 +0200

    Clase Model y Map para envío de dependencias con Thymeleaf

--- src/main/java/com/juan/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/juan/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -1,5 +1,27 @@
 package com.juan.curso.springboot.webapp.springboot_web.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
 
+    @GetMapping("/details")
+    public String details (Model model) {
+
+        model.addAttribute("title", "Hola Mundo Spring Boot");
+        model.addAttribute("name", "Juan");
+        model.addAttribute("lastname", "Cruz");
+        return "details";
+    }
+
+    //@GetMapping("/details")
+    // public String details (Map model) {
+
+    //     model.put("title", "Hola Mundo Spring Boot");
+    //     model.put("name", "Juan");
+    //     model.put("lastname", "Cruz");
+    //     return "details";
+    // }
 }
