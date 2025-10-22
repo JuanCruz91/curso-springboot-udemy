e1e0dd84a2c54a0c94d5871da151884721be6a62
juanchitovilla2 <104386449+juanchitovilla2@users.noreply.github.com>
2025-10-22 19:49:10 +0200

    feat: Implemento método request para obtener parámetros de la URL con objeto HttpServletRequest

--- src/main/java/com/juan/curso/springboot/webapp/springboot_web/controllers/RequestParamsController.java
+++ src/main/java/com/juan/curso/springboot/webapp/springboot_web/controllers/RequestParamsController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.juan.curso.springboot.webapp.springboot_web.models.dto.ParamDto;
 import com.juan.curso.springboot.webapp.springboot_web.models.dto.ParamMixDto;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/api/params")
 public class RequestParamsController {
@@ -29,6 +31,21 @@ public class RequestParamsController {
         params.setCode(code);
         return params;
     }
+
+    @GetMapping("/request")
+    public ParamMixDto request(HttpServletRequest request) {
+
+        Integer code = 0;
+        try {
+            Integer.parseInt(request.getParameter("code"));
+        } catch (NumberFormatException e) {
+        }
+        ParamMixDto params = new ParamMixDto();
+        params.setCode(code);
+        params.setMessage(request.getParameter("message"));
+        return params;
+    }
+    
     
 }
 
