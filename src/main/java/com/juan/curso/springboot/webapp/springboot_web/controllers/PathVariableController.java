5a66b4bc36c87cb69b97a36853383d7811e94b34
juanchitovilla2 <104386449+juanchitovilla2@users.noreply.github.com>
2025-10-23 18:35:39 +0200

    feat: Implemento método create que captura el JSON de la petición POST y lo mapea en un objeto User para manejar los datos

--- src/main/java/com/juan/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
+++ src/main/java/com/juan/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
@@ -3,6 +3,7 @@ package com.juan.curso.springboot.webapp.springboot_web.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.juan.curso.springboot.webapp.springboot_web.models.User;
 import com.juan.curso.springboot.webapp.springboot_web.models.dto.ParamDto;
 
 import java.util.HashMap;
@@ -10,6 +11,9 @@ import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 
@@ -35,5 +39,12 @@ public class PathVariableController {
         json.put("id", id);
         return json;
     }
+
+    @PostMapping("/create")
+    public User create(@RequestBody User user) {
+        user.setName(user.getName().toUpperCase());
+        return user;
+    }
+    
     
 }
